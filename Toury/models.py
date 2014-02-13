072f933ec1988d89b408d5f00d40def3ca3c9f3b
xran-deex <randy.valis@gmail.com>
2014-02-12 22:09:42 -0500

    created model

--- Toury/models.py
+++ Toury/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Marker(models.Model):
+    latitude = models.DecimalField(decimal_places=17, max_digits=20)
+    longitude = models.DecimalField(decimal_places=17, max_digits=20)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return "Lat: " + str(self.latitude) + "\n" + "Long: " + str(self.longitude) + "\n" + self.description
