24eb62940a9c0ae85564a2c7aa399113132d6716
xran-deex <randy.valis@gmail.com>
2014-02-14 17:24:35 -0500

    updated model to match Geofences

--- Toury/models.py
+++ Toury/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Marker(models.Model):
     latitude = models.DecimalField(decimal_places=17, max_digits=20)
     longitude = models.DecimalField(decimal_places=17, max_digits=20)
+    radius = models.DecimalField(decimal_places=15, max_digits=20)
+    title = models.CharField(max_length=50)
     description = models.TextField()
 
     def __unicode__(self):
