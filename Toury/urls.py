a5d14c87f1991b872d626b889a3fc5ee3206362b
xran-deex <randy.valis@gmail.com>
2014-02-22 21:07:51 -0500

    added geolocation to map and submit functionality

--- Toury/urls.py
+++ Toury/urls.py
@@ -5,5 +5,7 @@ from Toury import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^Tours$', views.tours, name='tours'),
+    url(r'^tours$', views.tours, name='tours'),
+    url(r'^add', views.add_marker, name='add_marker'),
+    url(r'^success', views.success),
 )
--- Toury/views.py
+++ Toury/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User, Group
+from django.shortcuts import render, redirect
+from django.core.context_processors import csrf
 from rest_framework import viewsets
 from serializers import *
 from Toury import models
@@ -9,6 +9,25 @@ def index(request):
 
 def tours(request):
     return render(request, 'Toury/tours.html', None)
+
+def add_marker(request):
+
+    if request.method != 'POST':
+        return;
+    else:
+        m = models.Marker()
+        m.description = request.POST['description']
+        m.direction = 0.0
+        m.latitude = request.POST['latitude']
+        m.longitude = request.POST['longitude']
+        m.radius = 5.0;
+        m.title = request.POST['title']
+        m.save()
+        return redirect('/success')
+
+def success(request):
+    return render(request, 'Toury/success.html', None)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
